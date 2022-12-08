b5b64fda921cf2da754285f2495debb5e82ed26d
an-rodriguez <an-rodriguez@globant.com>
2022-12-08 18:45:57 -0300

    fix(postgres): use .env file in confs

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,9 @@
 import os
-
+from dotenv import load_dotenv
+from pathlib import Path
 from pydantic import BaseSettings
 
+load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Items API"
@@ -11,14 +13,13 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT: str = os.getenv(
-        "POSTGRES_PORT", 5432
+        "POSTGRES_PORT", "5432"
     )  # default postgres port is 5432
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
-    # SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
--- app/db/session.py
+++ app/db/session.py
@@ -3,12 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-# SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-
-
 engine = create_engine(
     settings.SQLALCHEMY_DATABASE_URI,
     # required for sqlite
-    connect_args={"check_same_thread": False},
+    # connect_args={"check_same_thread": False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
