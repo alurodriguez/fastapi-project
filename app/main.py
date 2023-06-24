9fb1c4868335cc3cd1c68cbf64ece0b73be7ed95
Alexander Rodriguez <alerodriguezquiroga@gmail.com>
2023-06-24 17:41:12 -0300

    feat(prestart): add prestart scripts

--- app/main.py
+++ app/main.py
@@ -14,5 +14,5 @@ app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(login.router)
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
