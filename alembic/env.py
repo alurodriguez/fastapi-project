f47b82e83d54df151acf6ed5501ec4f2a35f8fdb
Alexander Rodriguez <alerodriguezquiroga@gmail.com>
2023-06-25 20:17:53 -0300

    refactor(alembic): remove unused code

--- alembic/env.py
+++ alembic/env.py
@@ -6,11 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 import os, sys
-from dotenv import load_dotenv
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-sys.path.append(BASE_DIR)
+from app.core import settings  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +14,8 @@ config = context.config
 
 # this will overwrite the ini-file sqlalchemy.url path
 # with the path given in the config of the main code
-config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+#config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -38,6 +35,14 @@ target_metadata = Base.metadata
 # ... etc.
 
 
+# def get_url():
+#     user = os.getenv("POSTGRES_USER", "postgres")
+#     password = os.getenv("POSTGRES_PASSWORD", "")
+#     server = os.getenv("POSTGRES_SERVER", "db")
+#     db = os.getenv("POSTGRES_DB", "app")
+#     return f"postgresql://{user}:{password}@{server}/{db}"
+
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -50,7 +55,9 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = settings.SQLALCHEMY_DATABASE_URI
+    # url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
